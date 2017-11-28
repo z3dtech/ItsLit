b0899b1bc52e112bf9ee17454236b71a9b83ea61
Zed <20992790+zed-apps@users.noreply.github.com>
2017-11-27 23:38:52 -0800

    progress

--- python/read.py
+++ python/read.py
@@ -1,8 +1,13 @@
 import serial
 import serial.tools.list_ports
 import json
+import time
 from StoPy import StoPy
 from multiprocessing import Pool
+
+sto = StoPy.StoPy( "localhost", "8080", "cbab68f9e00f1ffc58f198e042064d54" ) 
+
+# this function generates an ID for the Pi MCU
 
 def getPiId():
   cpuserial = "0000000000000000"
@@ -17,6 +22,9 @@ def getPiId():
   return "MCU-" + cpuserial
 
 
+
+# this function scans all USB ports for arduino connections and stores them
+
 def getArduinoPorts():
 	arduinos = []
 	arduino_labels = ('Arduino', 'FT231X')
@@ -26,34 +34,55 @@ def getArduinoPorts():
 	    	arduinos.append( {'port': p[0], 'arduino_id': p.serial_number } )
 	return arduinos
 
-#
+# this function listens to an individual arduino
 
 def parseInput( arduino, listen=9600 ):
 	print( "attempting to read arduino" )
 	print(arduino)
+	upload = { 'collection': getPiId(), 'owner': arduino['arduino_id'] }
 	i = 0
 	except_counter = 0
 	port = arduino['port']
 	ser = serial.Serial( port, listen, timeout=15 )
-	while i < 5:
+	while i < 4:
 		try: 
-			print( ser.read(255) )
+			lines = ser.read(255).split("\n")
+			#print( lines[len(lines)-2] )
+			upload['data'] = json.loads( lines[len(lines)-2] )
+			print( upload )
 			i += 1
+		except ValueError:  # includes simplejson.decoder.JSONDecodeError
+			print 'Decoding JSON has failed'
+			except_counter +=1
+			if except_counter == 10:
+				break
 		except serial.serialutil.SerialException:
-		    except_counter +=1
-		    if except_counter == 5:
-		       break
-		except KeyboardInterrupt:
-			break
+			print 'Serial Issue'
+			except_counter +=1
+			if except_counter == 5:
+				break
+		except IOError:
+			print 'Disconnected from USB'
+			except_counter +=1
+			if except_counter == 5:
+				break
+			sleep(20)
+
 	ser.flushInput()
 	ser.close()
 	return True
 
-#sto = StoPy.StoPy( "localhost", "8080", "api_key" ) 
-# should load this info from a hidden file
 arduinos_list = getArduinoPorts()
-collection = getPiId()
-print( "collection:" )
-#multiprocess
-while True:
-	map( parseInput, arduinos_list )
+except_counter = 0
+
+while True: 
+	try: # listen to each arduino
+		map( parseInput, arduinos_list ) #currently round robins on 30 second rotations
+		#should find a way to async multiprocess 
+		#pool().map_async wasn't working =_=
+	except serial.serialutil.SerialException:
+	    except_counter +=1
+	    if except_counter == 5:
+	       break
+	except KeyboardInterrupt:
+		break
